82ef28aba0b3a7a5f85b28342c2736aaf936e9b5
unknown <farolpublicidad@gmail.com>
2019-07-27 23:14:09 -0500

    nex routine

---
+++ general-program-php/general-function.php
@@ -0,0 +1,19 @@
+<?php
+
+function menu_nav($var_menu){
+    echo '    
+    <nav style="
+    color: red;
+    display: flex;
+    border: 5px solid red;
+    
+    ">
+     <ol>
+          <a href="index-primero.php">hello</a>
+      </ol>
+     <ol>
+          <a>world</a>
+      </ol>
+    </nav>';
+};
+
